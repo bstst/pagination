93f9f5ae32c74b2b4128b5b4011892484dc28ad7
Martin Saulis <martin@saulis.com>
2013-12-20 22:56:25 +0200

    lost in context

--- pagination.js
+++ pagination.js
@@ -1,81 +1,79 @@
-// dependencies: jquery, icanhaz
-
 var Pagination = (function(){
-    function Pagination(opts){
-        if(!(this instanceof Pagination)){
-            throw new Error('instantiate me!');
-        } else {
-            opts = opts || {};
-            initialize(opts);
-        }
-    }
-
-    var initialize = function(opts){
-        p.opts = opts;
-		if(!ich.pagination){
-			ich.addTemplate('pagination', '<div class="pagination"><ul>{{#pages}}<li class="{{active}} {{className}}"><a href="" data-page="{{href}}">{{title}}</a></li>{{/pages}}</ul></div>');
+	function Pagination(opts){
+		if(!(this instanceof Pagination)){
+			throw new Error('instantiate me!');
+		} else {
+			opts = opts || {};
+			initialize(opts);
 		}
-        p.padding = opts.padding || 3;
+	}
 
-        p.opts.el.after(p.$el);
+	var initialize = function(opts){
+		p.opts = opts;
+		ich.addTemplate('pagination', '<div class="pagination"><ul>{{#pages}}<li class="{{active}}"><a href="" data-page="{{href}}">{{title}}</a></li>{{/pages}}</ul></div>');
+		p.padding = opts.padding || 3;
 
-        generatePages();
+		p.opts.el.after(p.$el);
 
-        p.$el.on('click', 'a', function(e){
-			e.preventDefault();
-            var $a = $(this);
-            var page = $a.data('page');
+		generatePages();
 
-            p.$el.find('li.active').removeClass('active');
-            p.setPage(page);
+		p.$el.on('click', 'a', function(){
+			var $a = $(this);
+			var page = $a.data('page');
 
-            generatePages();
+			p.$el.find('li.active').removeClass('active');
+			p.setPage(page);
 
-            if(opts.callback){
-                opts.callback(page);
-            }
-            return false;
-        });
-    };
+			generatePages();
 
-    var generatePages = function(){
-        var pages = [];
-        var onPage = p.onPage;
-        var padding = p.padding;
-        var totalPages = Math.ceil(p.opts.totalItems / p.opts.perPage);
+			if(opts.callback){
+				opts.callback(page);
+			}
+			return false;
+		});
+	};
 
-        var from = Math.max(0, onPage - padding);
-        var to = Math.min(totalPages, onPage + padding + 1);
+	var generatePages = function(){
+		var pages = [];
+		var onPage = p.onPage;
+		var padding = p.padding;
+		var totalPages = Math.ceil(p.opts.totalItems / p.opts.perPage);
 
-        if(onPage - from < padding){
-            to += Math.min(totalPages, padding - (onPage - from));
-        } else if(to - onPage < padding){
-            from -= Math.max(0, padding - (to - onPage));
-        }
 
-        var first = onPage === 0;
-        pages.push({href: 0, title: '«', active: first ? 'active' : '', className: 'first'});
-        pages.push({href: Math.max(0, onPage - 1), title: '‹ Previous', active: first ? 'active' : '', className: 'prev'});
+		var from = Math.max(0, onPage - padding);
+		var to = Math.min(totalPages, onPage + padding + 1);
 
-        for(var i = from; i < to; i++){
-            pages.push({href: i, title: i + 1, active: i === onPage ? 'active' : '', className: 'page'});
-        }
 
-        var last = onPage === Math.min(totalPages - 1, onPage + 1);
-        pages.push({href: Math.min(totalPages - 1, onPage + 1), title: 'Next ›', active: last ? 'active' : '', className: 'next'});
-        pages.push({href: totalPages - 1, title: '»', active: last ? 'active' : '', className: 'last'});
+		if(onPage - from < padding){
+			//WTF???
+			// to += Math.min(totalPages, padding - (onPage - from));
+		} else if(to - onPage < padding){
+			from -= Math.max(0, padding - (to - onPage));
+		}
 
-        p.$el.html(ich.pagination({pages: pages}));
-    };
+		var first = onPage === 0;
+		pages.push({href: 0, title: 'Â«', active: first ? 'active' : ''});
+		pages.push({href: Math.max(0, onPage - 1), title: 'â€¹ Previous', active: first ? 'active' : ''});
 
-    var p = Pagination.prototype;
+		for(var i = from; i < to; i++){
+			pages.push({href: i, title: i + 1, active: i === onPage ? 'active' : ''});
+		}
 
-    p.$el = $('<div></div>');
-    p.onPage = 0;
-    p.setPage = function(page){
-        this.onPage = page;
-        this.$el.find('[data-page=' + page + ']').closest('li').addClass('active');
-    };
+		var last = onPage === Math.min(totalPages - 1, onPage + 1);
+		pages.push({href: Math.min(totalPages - 1, onPage + 1), title: 'Next â€º', active: last ? 'active' : ''});
+		pages.push({href: totalPages - 1, title: 'Â»', active: last ? 'active' : ''});
 
-    return Pagination;
+		p.$el.html(ich.pagination({pages: pages}));
+	};
+
+	var p = Pagination.prototype;
+
+	p.$el = $('<div></div>');
+	p.onPage = 0;
+	p.setPage = function(page){
+		this.onPage = page;
+		this.$el.find('[data-page=' + page + ']').closest('li').addClass('active');
+	};
+
+	return Pagination;
 })();
